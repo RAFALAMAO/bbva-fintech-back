fa3047b021d2e9a2bf33b85d1e138ffd1b28ba25
RAFALAMAO <aaronjt@outlook.com>
2023-01-16 12:43:34 -0600

    Update cors

--- src/config/cors.js
+++ src/config/cors.js
@@ -2,7 +2,7 @@ import { environment_prod } from "../config";
 
 const whiteList = environment_prod
   ? ["http://20.114.144.5/"]
-  : ["https://develop.lendinc.mx", "http://localhost:3000", "http://localhost:3001", "http://localhost:3002"];
+  : ["https://develop.lendinc.mx", "http://localhost:3000", "http://localhost:3001", "http://localhost:3002", "http://127.0.0.1/"];
 
 const corsOptions = {
     origin: function(origin, callback){
